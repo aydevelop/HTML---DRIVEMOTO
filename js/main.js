d629bbaf089e42068648326d013450d8879944ba
aydevelop <ay.develop1@gmail.com>
2020-11-03 21:50:41 +0200

    07

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-$(function(){
-
-});
+$(function () {
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow:
+      '<bottom class="banner-section__slider-btn banner-section__slider-btnprev">###</bottom>',
+  })
+})
