f8076dc1b258485b0b338b9cca0814a573a789fd
aydevelop <ay.develop1@gmail.com>
2020-11-04 21:32:10 +0200

    08

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(function () {
   $('.banner-section__slider').slick({
     dots: true,
-    prevArrow:
-      '<bottom class="banner-section__slider-btn banner-section__slider-btnprev">###</bottom>',
+    prevArrow: `<bottom class="banner-section__slider-btn banner-section__slider-btnprev">
+     <img src="images/arrow-left.svg" >  
+    </bottom>`,
+    nextArrow: `<bottom class="banner-section__slider-btn banner-section__slider-btnnext">
+     <img src="images/arrow-right.svg" >  
+    </bottom>`,
   })
 })
