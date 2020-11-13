4095e89c342e7311495ed3c0fafcb428bf6a26a4
aydevelop <ay.develop1@gmail.com>
2020-11-13 12:17:01 +0200

    Popular goods completed

--- js/main.js
+++ js/main.js
@@ -24,5 +24,19 @@ $('.search__tabs-item').on('click', function (e) {
 $('.product-item__favorite').on('click', function (e) {
   e.preventDefault()
 
-  $('.product-item__favorite').toggleClass('product-item__favorite--active')
+  $('.product-item__favorite').removeClass('product-item__favorite--active')
+  $(this).addClass('product-item__favorite--active')
+})
+
+$('.product-slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  dots: true,
+  prevArrow: `<bottom class="banner-section__slider-btn banner-section__slider-btnprev banner-section__slider-btnprev--product">
+   <img src="images/arrow4.svg" >  
+  </bottom>`,
+  nextArrow: `<bottom class="banner-section__slider-btn banner-section__slider-btnnext banner-section__slider-btnnext--product">
+   <img src="images/arrow3.svg" >  
+  </bottom>`,
 })
